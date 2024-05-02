4196a2f83a25fbd6bcdd6e40eb66a934f711ce94
Yari Torres Nicola <ybtorres9@gmail.com>
2024-05-01 23:22:25 -0400

    added basic homepage and welcome, will edit CSS in future

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Homepage from "./pages/Homepage";
 
 export default function Home() {
   return (
     <main>
+      <Homepage />
     </main>
   );
 }
---
+++ src/app/pages/Homepage.tsx
@@ -0,0 +1,9 @@
+import '../styles/Homepage.css';
+
+export default function Homepage() {
+  return (
+    <div>
+      Welcome to Nanifesto.
+    </div>
+  )
+}
