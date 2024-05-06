08ed300c4353bc0e09da2e9a0d37e120ceb5bb46
Yari Torres Nicola <ybtorres9@gmail.com>
2024-05-06 18:30:31 -0400

    added minor styles

--- src/app/pages/Homepage.tsx
+++ src/app/pages/Homepage.tsx
@@ -2,8 +2,13 @@ import '../styles/Homepage.css';
 
 export default function Homepage() {
   return (
-    <div>
-      Welcome to Nanifesto.
+    <div className='w-screen h-screen bg-slate-100 content-center justify-center'>
+      <div className='bg-green-900 border-solid border-4 w-2/4 h-1/4 items-center'>
+        <h1 className='font-mono text-6xl text-center text-lime-400'>
+          welcome to nanifesto.
+        </h1>
+      </div>
     </div>
+
   )
 }
