4a65dfe95e58de4654c837934a8384eb2cea7073
Yari Torres Nicola <ybtorres9@gmail.com>
2024-05-06 19:37:07 -0400

    added basic on click animation for home screen button

--- src/app/pages/Homepage.tsx
+++ src/app/pages/Homepage.tsx
@@ -1,9 +1,18 @@
+'use client'
 import '../styles/Homepage.css';
+import { useState } from 'react';
 
 export default function Homepage() {
+  const [animate, setAnimate] = useState(false);
   return (
-    <div className='w-screen h-screen bg-slate-100 content-center justify-center'>
-      <div className='bg-green-900 border-solid border-4 w-2/4 h-1/4 items-center'>
+    <div className='flex w-screen h-screen bg-slate-100 place-content-center place-items-center'>
+      <div
+        className={
+          `${animate && 'animate-ping'} flex border-green-900 border-solid border-4 rounded-lg w-2/4 h-1/4 place-items-center cursor-pointer transition hover:bg-green-900`
+        }
+        onClick={ () => setAnimate(true) }
+        onAnimationEnd={ () => setAnimate(false) }
+      >
         <h1 className='font-mono text-6xl text-center text-lime-400'>
           welcome to nanifesto.
         </h1>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        ping: 'ping 1s cubic-bezier(0, 0, 0.2, 1)'
+      }
     },
   },
   plugins: [],
