2973b431f9bf802a3e9cc6f9127f6da09133ea95
Yari Torres Nicola <ybtorres9@gmail.com>
2024-06-11 18:41:45 -0400

    using axios to access the express server's page locally succeeded and i am finally able to display data from my database on the front endgit status now to add a way to post data to the database and I'm all set on MVP

--- src/app/view-posts/page.tsx
+++ src/app/view-posts/page.tsx
@@ -1,9 +1,36 @@
 'use client'
 import { useEffect, useState } from "react";
 import HamburgerMenu from "../components/hamburgerMenu";
+const axios = require('axios').default;
 
 export default function ViewPosts() {
   const [hamOpen, setHamOpen] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      url: 'http://localhost:3000/posts',
+      headers: {}
+    };
+
+    const retrievePosts = () => {
+      return axios.get(options.url, {headers: options.headers})
+      .then(database => {
+        setPosts(database.data);
+        return database.data;
+      })
+      .then(fetchedPosts => {
+        console.log('fetched:', fetchedPosts);
+        console.log('posts:', posts);
+        // setPosts(fetchedPosts);
+      })
+      .catch(err => console.log('API ERROR:', err))
+    }
+
+    retrievePosts();
+  }, [JSON.stringify(posts)])
+
+
 
   useEffect(() => {
     const savedMode = window.localStorage.getItem('theme');
@@ -30,14 +57,18 @@ export default function ViewPosts() {
             `flex flex-col border-green-900 border-solid border-4 rounded-lg w-5/6 h-5/6 place-items-center place-content-center          overflow-y-auto space-y-2 p-8`
           }
         >
+        {posts.map((post, postId) =>
           <li
+            key={postId}
             className={
-              `flex rounded bg-lime-400 font-mono text-2xl w-full h-1/6 justify-start items-center p-4
-              ${ hamOpen ? 'hover:cursor-default' : 'hover:cursor-pointer transition-all hover:bg-lime-500' }`
+              `rounded bg-lime-400 font-mono text-2xl w-full h-auto justify-start p-4 space-y-4`
             }
           >
-            henlo
+            <label>{post.title}</label>
+            <p>{post.body}</p>
+            <span>posted: {post.date_posted}</span>
           </li>
+        )}
         </ul>
       </div>
     </>
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -2,14 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const postgres = require('./../database/db.tsx');
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(process.env.PORT);
 console.log(`Listening on port ${process.env.PORT}`);
 
-app.get(`/`, (req, res) => {
+app.get(`/posts`, (req, res) => {
   postgres.getPosts()
   .then(data => {
     res.send(data.rows);
